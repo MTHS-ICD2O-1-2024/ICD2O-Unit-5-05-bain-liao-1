d193964131f179a6d49cf9bd4abf821db4ba1deb
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-05-02 00:06:33 +0000

    created program that determines the type of triangle

--- js/script.js
+++ js/script.js
@@ -4,25 +4,35 @@
 // Created on: Apr 2025
 // This file contains the JS functions for index.html
 
-"use scrict"
+"use strict"
 
+/**
+ * This function determines the type of triangle.
+ */
 function determineTypeOfTriangle() {
-  // input
-  const lengthA = parseInt(document.getElementById('length-a').value)
-  const lengthB = parseInt(document.getElementById('length-b').value)
-  const lengthC = parseInt(document.getElementById('length-c').value)
+  const lengthA = parseInt(document.getElementById("length-a").value);
+  const lengthB = parseInt(document.getElementById("length-b").value);
+  const lengthC = parseInt(document.getElementById("length-c").value);
+
 
   // using the cosine law
-  const angleA = Math.acos((lengthB**2 + lengthC**2 - lengthA**2) / (2 * lengthB * lengthC)) * (180/Math.PI)
-  const angleB = Math.acos((lengthC**2 + lengthA**2 - lengthB**2) / (2 * lengthC * lengthA)) * (180/Math.PI)
-  const angleC = Math.acos((lengthA**2 + lengthB**2 - lengthC**2) / (2 * lengthA * lengthB)) * (180/Math.PI)
+  const angleA = Math.acos((lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)) * (180 / Math.PI);
+  const angleB = Math.acos((lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)) * (180 / Math.PI);
+  const angleC = Math.acos((lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)) * (180 / Math.PI);
+  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2));
 
-  const sumOfAngles = Number((angleA).toFixed(2)) + Number((angleB).toFixed(2)) + Number((angleC).toFixed(2))
+  console.log(angleA);
+  console.log(angleB);
+  console.log(angleC);
+  console.log(sumOfAngles);
 
-  // process
-  if (sumOfAngles != 180) {
-    document.getElementById('result').innerHTML =
-      '<p>Your shape is not a triangle</p>'
+  if (sumOfAngles != 180 || (lengthA + lengthB <= lengthC || lengthA + lengthC <= lengthB || lengthB + lengthC <= lengthA)) {
+    document.getElementById("result").innerText = "The triangle is not a real triangle.";
+  } else if (lengthA == lengthB && lengthB == lengthC && lengthA == lengthC) {
+    document.getElementById("result").innerText = "The triangle is equilateral.";
+  } else if (lengthA != lengthB && lengthB != lengthC && lengthA != lengthC) {
+    document.getElementById("result").innerText = "The triangle is scalene.";
+  } else {
+    document.getElementById("result").innerText = "The triangle is isosceles.";
   }
-
 }
